db4eb95c2cf863404ef6126078fc4628489361c6
timothyzhong <timothy7784@gmail.com>
2015-10-17 20:55:44 -0500

    Implement swissPairings function

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -42,17 +42,17 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
     conn = connect()
     cursor = conn.cursor()
     sql = "insert into players values(%s);"
-    cursor.execute(sql,(name,))
+    cursor.execute(sql, (name,))
     conn.commit()
     conn.close()
 
@@ -60,8 +60,8 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -82,13 +82,13 @@ def playerStandings():
         player_id = row[0]
         name = row[1]
         wins = row[2]
-        if wins==None:
-            wins=0
+        if wins is None:
+            wins = 0
         loses = row[3]
-        if loses==None:
-            loses=0
-        matches = wins+loses 
-        result.append([player_id,name,wins,matches])
+        if loses is None:
+            loses = 0
+        matches = wins + loses
+        result.append([player_id, name, wins, matches])
     return result
 
 
@@ -101,46 +101,45 @@ def reportMatch(winner, loser):
     """
     conn = connect()
     cursor = conn.cursor()
-    
-    #update winner
+
+    # update winner
     sql = "select win from matches where id=%s"
-    cursor.execute(sql,(winner,))
+    cursor.execute(sql, (winner,))
     row = cursor.fetchone()
-    if(row == None):		#not exist
-        sql = "insert into matches values(%s,1,0)"	
-        cursor.execute(sql,(winner,))
-    else:                       #update existed row
+    if(row is None):		# not exist
+        sql = "insert into matches values(%s,1,0)"
+        cursor.execute(sql, (winner,))
+    else:                       # update existed row
         winCount = row[0]
         winCount += 1
         sql = "update matches set win=%s where id=%s"
-        cursor.execute(sql,(winCount,winner,))
+        cursor.execute(sql, (winCount, winner,))
 
-    #update loser
+    # update loser
     sql = "select lose from matches where id=%s"
-    cursor.execute(sql,(loser,))
+    cursor.execute(sql, (loser,))
     row = cursor.fetchone()
-    if(row == None):
-        sql = "insert into matches values(%s,0,1)"    
-        cursor.execute(sql,(loser,))
+    if(row is None):
+        sql = "insert into matches values(%s,0,1)"
+        cursor.execute(sql, (loser,))
     else:
         loseCount = row[0]
         loseCount += 1
-        sql = "update matches set lose=%s where id=%s" 
-        cursor.execute(sql,(loseCount,loser,)) 
- 
+        sql = "update matches set lose=%s where id=%s"
+        cursor.execute(sql, (loseCount, loser,))
+
     conn.commit()
     conn.close()
-    
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -148,5 +147,23 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    conn = connect()
+    cursor = conn.cursor()
 
-
+    sql = "select players.id,name,win\
+          from players,matches\
+          where players.id=matches.id\
+          order by win desc;"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    players = []
+    for row in rows:
+        players.append((row[0], row[1]))
+    pairs = []
+    x = 0
+    while x < len(players):
+        pairs.append((players[x][0], players[x][1],
+                      players[x+1][0], players[x+1][1]))
+        x += 2
+    conn.close()
+    return pairs
